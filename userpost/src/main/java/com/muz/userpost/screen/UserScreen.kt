dad6c05d14b4f00c9d3bb212ede9b8f62706741c
Muz <smuzani@gmail.com>
2024-12-16 19:34:12 +0800

    main screen should be cards

--- userpost/src/main/java/com/muz/userpost/screen/UserScreen.kt
+++ userpost/src/main/java/com/muz/userpost/screen/UserScreen.kt
@@ -1,7 +1,11 @@
 package com.muz.userpost.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,59 +27,59 @@ import com.muz.userpost.network.UserPost
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UserScreen(
-    userPost: UserPost
+  userPost: UserPost
 ) {
-    LazyColumn {
-        // Header image
-        item {
-            Image(
-                painter = rememberAsyncImagePainter(userPost.user?.url),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(240.dp)
-                    .semantics { testTagsAsResourceId = true }
-                    .testTag("userImage"),
-                contentScale = ContentScale.Crop
-            )
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-
-        // Posts
-        items(userPost.posts ?: emptyList()) { post ->
-            PostCard(post)
-            Spacer(modifier = Modifier.height(8.dp))
-        }
+  LazyColumn {
+    // Header image
+    item {
+      Image(
+        painter = rememberAsyncImagePainter(userPost.user?.url),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(240.dp)
+            .semantics { testTagsAsResourceId = true }
+            .testTag("userImage"),
+        contentScale = ContentScale.Crop
+      )
+      Spacer(modifier = Modifier.height(16.dp))
     }
+
+    // Posts
+    items(userPost.posts ?: emptyList()) { post ->
+      PostCard(post)
+      Spacer(modifier = Modifier.height(8.dp))
+    }
+  }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 private fun PostCard(post: Post) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .semantics { testTagsAsResourceId = true }
-            .testTag("postCard"),
-        shape = RoundedCornerShape(8.dp)
+  Card(
+    modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp)
+        .semantics { testTagsAsResourceId = true }
+        .testTag("postCard"),
+    shape = RoundedCornerShape(8.dp)
+  ) {
+    Column(
+      modifier = Modifier
+          .fillMaxWidth()
+          .padding(16.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            Text(
-                text = post.title ?: "",
-                style = MaterialTheme.typography.titleLarge
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = post.body ?: "",
-                style = MaterialTheme.typography.bodyLarge
-            )
-        }
+      Text(
+        text = post.title ?: "",
+        style = MaterialTheme.typography.titleLarge
+      )
+      Spacer(modifier = Modifier.height(8.dp))
+      Text(
+        text = post.body ?: "",
+        style = MaterialTheme.typography.bodyLarge
+      )
     }
+  }
 }
 
 const val USER_DETAILS = "UserScreen"
--- userpost/src/main/java/com/muz/userpost/screen/UsersScreen.kt
+++ userpost/src/main/java/com/muz/userpost/screen/UsersScreen.kt
@@ -2,13 +2,20 @@ package com.muz.userpost.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -28,68 +35,69 @@ import com.muz.userpost.network.UserPost
 
 @Composable
 fun UsersScreen(
-    onNavigateToDetails: (UserPost) -> Unit,
-    vm: UserViewModel
+  onNavigateToDetails: (UserPost) -> Unit,
+  vm: UserViewModel
 ) {
-    val userPostsState by vm.userPosts.collectAsState()
+  val userPostsState by vm.userPosts.collectAsState()
 
-    LazyColumn {
-        items(userPostsState, key = { userPost -> userPost.user?.name ?: "" }) { userPost ->
-            UserRow(userPost, onNavigateToDetails)
-            HorizontalDivider()
-        }
+  LazyColumn {
+    items(userPostsState, key = { userPost -> userPost.user?.name ?: "" }) { userPost ->
+      UserRow(Modifier.padding(horizontal = 12.dp, vertical = 4.dp), userPost, onNavigateToDetails)
     }
+  }
 }
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun UserRow(userPost: UserPost, onNavigateToDetails: (UserPost) -> Unit) {
+fun UserRow(modifier: Modifier, userPost: UserPost, onNavigateToDetails: (UserPost) -> Unit) {
+  Card(modifier = modifier, shape = RoundedCornerShape(12.dp)) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 8.dp)
-            .clickable { onNavigateToDetails(userPost) },
-        verticalAlignment = Alignment.CenterVertically
+      modifier = Modifier
+        .fillMaxWidth()
+        .padding(horizontal = 16.dp, vertical = 8.dp)
+        .clickable { onNavigateToDetails(userPost) },
+      verticalAlignment = Alignment.CenterVertically
     ) {
-        Image(
-            painter = rememberAsyncImagePainter(
-                model = userPost.user?.thumbnailUrl
-            ),
-            contentDescription = null,
-            modifier = Modifier
-                .size(100.dp)
-                .clip(CircleShape)
-                .semantics { testTagsAsResourceId = true }
-                .testTag("thumbnail")
-        )
+      Image(
+        painter = rememberAsyncImagePainter(
+          model = userPost.user?.thumbnailUrl
+        ),
+        contentDescription = null,
+        modifier = Modifier
+          .size(100.dp)
+          .clip(CircleShape)
+          .semantics { testTagsAsResourceId = true }
+          .testTag("thumbnail")
+      )
 
-        Column(
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 16.dp)
-        ) {
-            Text(
-                text = userPost.user?.name ?: "",
-                style = MaterialTheme.typography.headlineLarge
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-            Text(
-                text = "${userPost.postCount} posts",
-                style = MaterialTheme.typography.headlineSmall,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
-            )
-        }
-
-        Icon(
-            modifier = Modifier
-                .semantics { testTagsAsResourceId = true }
-                .testTag("caret")
-                .height(48.dp),
-            imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
-            contentDescription = null,
-            tint = MaterialTheme.colorScheme.onSurfaceVariant
+      Column(
+        modifier = Modifier
+          .weight(1f)
+          .padding(start = 16.dp)
+      ) {
+        Text(
+          text = userPost.user?.name ?: "",
+          style = MaterialTheme.typography.headlineLarge
         )
+        Spacer(modifier = Modifier.height(12.dp))
+        Text(
+          text = "${userPost.postCount} posts",
+          style = MaterialTheme.typography.headlineSmall,
+          color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+      }
+
+      Icon(
+        modifier = Modifier
+          .semantics { testTagsAsResourceId = true }
+          .testTag("caret")
+          .height(48.dp),
+        imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+        contentDescription = null,
+        tint = MaterialTheme.colorScheme.onSurfaceVariant
+      )
     }
+  }
 }
 
 const val USERS = "UsersScreen"
