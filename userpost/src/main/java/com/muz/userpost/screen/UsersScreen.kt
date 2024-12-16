964f0d85ff774fdd1708296d6ae89dbc84e689dd
Muz <smuzani@gmail.com>
2024-12-16 19:02:30 +0800

    main screen

--- userpost/src/main/java/com/muz/userpost/screen/UsersScreen.kt
+++ userpost/src/main/java/com/muz/userpost/screen/UsersScreen.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,8 +24,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.muz.spine.Nerve
@@ -64,35 +71,54 @@ fun UsersScreen(
   }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun UserRow(userPost: UserPost, onNavigateToDetails: (UserPost) -> Unit) {
   Row(
     modifier = Modifier
       .fillMaxWidth()
-      .padding(vertical = 12.dp)
-      .clickable {
-        onNavigateToDetails(userPost)
-      },
+      .padding(horizontal = 16.dp, vertical = 8.dp)
+      .clickable { onNavigateToDetails(userPost) },
     verticalAlignment = Alignment.CenterVertically
   ) {
     Image(
-      painter = rememberAsyncImagePainter(userPost.user?.thumbnailUrl),
-      contentDescription = "User photo",
+      painter = rememberAsyncImagePainter(
+        model = userPost.user?.thumbnailUrl
+      ),
+      contentDescription = null,
       modifier = Modifier
-        .size(64.dp)
+        .size(100.dp)
         .clip(CircleShape)
+        .semantics { testTagsAsResourceId = true }
+        .testTag("thumbnail")
     )
-    Spacer(modifier = Modifier.width(25.dp))
-    Column {
+
+    Column(
+      modifier = Modifier
+        .weight(1f)
+        .padding(start = 16.dp)
+    ) {
       Text(
         text = userPost.user?.name ?: "",
         style = MaterialTheme.typography.headlineLarge
       )
+      Spacer(modifier = Modifier.height(12.dp))
       Text(
-        text = "Posts: ${userPost.postCount}",
-        style = MaterialTheme.typography.bodyMedium
+        text = "${userPost.postCount} posts",
+        style = MaterialTheme.typography.headlineSmall,
+        color = MaterialTheme.colorScheme.onSurfaceVariant
       )
     }
+
+    Icon(
+      modifier = Modifier
+        .semantics { testTagsAsResourceId = true }
+        .testTag("caret")
+        .height(48.dp),
+      imageVector = Icons.AutoMirrored.Filled.KeyboardArrowRight,
+      contentDescription = null,
+      tint = MaterialTheme.colorScheme.onSurfaceVariant
+    )
   }
 }
 
